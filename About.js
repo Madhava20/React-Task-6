261dc1d5a1f4a826da96ecb6a69e1f01076dae4c
Madhava Reddy <92641734+Madhava20@users.noreply.github.com>
2023-10-23 17:16:40 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <section className="About">
+        <br></br>
+      <h2>About Me</h2>
+      <p>As a computer science student at Vellore Institute of Technology, I am passionate about programming and solving real-world problems with technology. I have a strong foundation in Python and C languages, and I have applied them to various projects and competitions, such as developing an app for campus navigation, creating a chatbot for mental health awareness, and participating in hackathons and coding contests. 
+
+I am also involved in several extracurricular activities that enhance my skills and knowledge in different domains. I am a technical team member at Android Club and Microsoft Student Chapter, where I learn and share best practices in mobile and web development. I am a scientific researcher at SEDS, where I conduct experiments and analysis on topics related to space exploration and sustainability. I am an event management team member at Uddeshya and Malayalam Association, where I organise and execute various cultural and social events for the student community. Through these experiences, I have developed my interpersonal, leadership, and critical thinking skills, and I have collaborated with people from diverse backgrounds and disciplines. 
+
+I am a motivated and hardworking individual who takes pride in being able to grasp new ideas and concepts quickly and effectively. I am always willing to learn and grow through my knowledge, and I seek challenging and rewarding opportunities as a computer science engineer. My goal is to contribute to the advancement and innovation of technology and society.
+      
+      </p>
+      <p>
+      I seek to earn valuable skills and good connections which help me succeed in my life. 
+I am seeking for guidance and mentorship in IT industry who help me get experience and 
+help me choose my domain for my future endeavours.</p>
+      <p>
+        When I'm not coding, you can find me exploring new design trends, enjoying a good cup of coffee, or traveling to new places.
+        I'm always open to new opportunities and collaborations. Feel free to reach out to me if you'd like to work together or just say hello!
+      </p>
+    </section>
+  );
+}
+
+export default About;
---
+++ Awards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Awards.css';
+
+function Awards() {
+  return (
+    <section className="awards">
+                <br></br>
+      <h2>Certifications</h2>
+      <ul>
+        <li>Programming for everybody (Coursera)</li>
+        <li>An Introduction to French Civilisation and Culture(VIT - AP)</li>
+        <li>Environmental Management &Corporate Social Responsibility(VIT - AP)</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Awards;
